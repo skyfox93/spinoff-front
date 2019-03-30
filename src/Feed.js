22f2fec5fbfaf5d6cc434b000dd8fa4b40fa5bbe
Skylar Salerno <skylar.salerno@gmail.com>
2019-03-30 14:55:05 -0400

    move not following warning

--- src/Feed.js
+++ src/Feed.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import PhotosContainer from './PhotosContainer'
 import {withRouter} from 'react-router-dom'
+import { Message } from 'semantic-ui-react'
+
 class  FeedViewer extends React.Component{
+  state= {
+    loading:false;
+  }
+
+  fetchFeed= () => {
+    this.setState({loading:true})
+    adapter.getFeed(this.props.user.id,this.token)
+    .then(photos => {
+      this.props.setPhotos(photos);
+      this.setState({loading:false, feedLoaded:true})}
+    )}
 
   componentDidMount(){
     this.props.fetchFeed()
@@ -9,13 +22,35 @@ class  FeedViewer extends React.Component{
 
   render(){
 
-  return  <div style={this.props.match.isExact ? {top:'50px',width:'100%', height:'90%', left:'0px', display: 'inline-block',position:'absolute',overflow: 'scroll',backgroundColor:'rgb(200,200,200)'}:{display:'none'} }>
-    <div style={{width:'700px', margin: '50px, auto', display: 'inline-block'}}>
-        <PhotosContainer
-        {...this.props}
+  return(
+    <div
+      style={this.props.match.isExact ? {
+        top:'50px',
+        width:'100%',
+        height:'90%',
+        left:'0px',
+        display: 'inline-block',
+        position:'absolute',
+        overflow:'scroll',
+        backgroundColor:'rgb(200,200,200)'
+      }
+        :{display:'none'} }>
+
+      <div style={{
+        width:'700px',
+        margin: '50px, auto',
+        display: 'inline-block'}}>
+
+    {(this.props.photos.length<1 && this.state.feedLoaded) ?
+   <Message>
+   <Message.Header> No posts to show </Message.Header>
+   You are not following anyone, or the people you are following haven't posted anything. Use the search bar above to find your friends. Once they accept your request, their photos will appear here.
+   </Message> : null}
+  <PhotosContainer
+        {...this.props}}
         />
       </div>
-    </div>
+    </div>)
   }
 }
 export default withRouter(FeedViewer)
--- src/PhotosContainer.js
+++ src/PhotosContainer.js
@@ -3,10 +3,13 @@ import Photo from './Photo2.js'
 import { Message } from 'semantic-ui-react'
 class PhotosContainer extends React.Component{
   render(){
-    if (this.props.photos.length<1 && this.props.loaded){
-    return <Message> <Message.Header> No posts to show </Message.Header> You are not following anyone, or the people you are following haven't posted anything. Use the search bar above to find your friends. Once they accept your request, their photos will appear here. </Message>}
-    else
-    {return this.props.photos.map((photo)=>
+
+    viewPhoto=(photo_id,history)=>{
+      props.viewPhoto(photo_id)
+     history.push('/profile/photo')}
+
+
+    return this.props.photos.map((photo)=>
       <Photo
       id={photo.id}
       owner={photo.owner}
