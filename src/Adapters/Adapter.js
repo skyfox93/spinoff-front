45bbd00d79075e902763107d758bec3a0388e094
Skylar Salerno <skylar.salerno@gmail.com>
2019-05-16 17:38:36 -0400

    Revert "heroku to VPS"
    This reverts commit c71c7a1e26c9e4161fd9bf3bab39db735a579875.

--- src/Adapters/Adapter.js
+++ src/Adapters/Adapter.js
@@ -124,8 +124,7 @@ function Adapter(baseUrl){
 
 }
 
-//const baseUrl='https://spinoff-back.herokuapp.com/api/v1'
-const baseUrl='http://174.138.58.32/spinoff/api/v1'
-//const baseUrl='https://spinoff-back.herokuapp.com/api/v1'
+const baseUrl='https://spinoff-back.herokuapp.com/api/v1'
+//const baseUrl='http://localhost:3000/api/v1'
 
 export default Adapter(baseUrl)
