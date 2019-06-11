3b672e1bcc264065ea0a8332eba14deea3253df7
Skylar Salerno <skylar.salerno@gmail.com>
2019-06-11 15:38:10 -0400

    fix toolbar display issues, add server for launching app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/spinoff',express.static(path.join(__dirname, 'build')));
+
+app.get('/spinoff/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3002);
