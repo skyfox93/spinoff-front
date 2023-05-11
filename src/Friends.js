caba897d8b12ef55dd330f9647a0e173325fd591
Skylar Salerno <skylar.salerno@gmail.com>
2023-05-11 12:57:48 -0400

    updates to loading users

--- src/Friends.js
+++ src/Friends.js
@@ -1,87 +1,77 @@
 import React from 'react'
 import adapter from './Adapter'
-import {Popup, Image, List, Button} from 'semantic-ui-react'
-import {Link } from 'react-router-dom'
-class Friends extends React.Component{
+import { Popup, Image, List, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+class Friends extends React.Component {
 
-  state= {
+  state = {
     // notReqResults= users that match the search,
     //to whom no follow request was made
-    noRelation:[],
-    following:[],
+    noRelation: [],
+    following: [],
     requested: [],
     searchLength: 0,
     loading: false
   }
-  requestFollow= (followeeId)=> {
-    let noRelation= this.state.noRelation.map(
-      user=>
-        user.id===followeeId ? {...user, requested:true} : user
+  requestFollow = (followeeId) => {
+    let noRelation = this.state.noRelation.map(
+      user =>
+        user.id === followeeId ? { ...user, requested: true } : user
     )
-      this.setState({noRelation})
-    adapter.requestFollow(this.props.userId,followeeId, this.props.token)
+    this.setState({ noRelation })
+    adapter.requestFollow(this.props.userId, followeeId, this.props.token)
 
   }
 
-  handleChange=(e)=> {
-    let value=e.target.value
-    if(value.length>1){
-      this.setState({loading:true})
-      adapter.search(this.props.userId,value,this.props.token)
-      .then(data=>{
-        console.log(data)
-        // if no results, set results to empty array
-        const noRelation= data.no_relation || []
-        const following= data.following || []
-        const requested= data.requested || []
-        this.setState({
-          noRelation,
-          following,
-          requested,
-          loading:false
-        })
-      })
-    }
-    else{this.setState({
-      noRelation:[],
-      following:[],
-      requested: [],
+  componentDidMount(){
+   this.search('')
+  }
+
+  handleChange = (e) => {
+    let value = e.target.value
+    this.setState({ loading: true })
+    this.search(value)
+  }
+
+  search = (value) => {
+    adapter.search(this.props.userId, value, this.props.token)
+    .then(data => {
+      // if no results, set results to empty array
+      this.setState({results: data})
     })
   }
-  }
-  results=()=> {
-    let baseUrl=this.props.baseUrl
-    return <List size='small'>
-      {this.state.following.map(
-        (result=><List.Item><List.Header><Image src={baseUrl+result.avatar.url} className='user-avatar' avatar /><Link to='/profile' onClick={()=>this.props.setViewingUser(result)}>{result.displayname}</Link><Button disabled floated='right'>Friends</Button></List.Header></List.Item>)
-      )}
-      {this.state.noRelation.map(
-        (result=>
-          <List.Item ><List.Header><Image src={this.props.baseUrl+result.avatar.url} className='user-avatar'  avatar />
-<Link to='/profile' onClick={()=>this.props.setViewingUser(result)}>{result.displayname}</Link><Button floated='right' onClick={()=>this.requestFollow(result.id)} >{result.requested ? "Requested": "Follow"}</Button></List.Header>
-</List.Item>
-      ))}
-      {this.state.requested.map(
-        (result=>
-          <List.Item><List.Header><Image src={this.props.baseUrl+result.avatar.url} className='user-avatar' avatar />
-<Link to='/profile' onClick={()=>this.props.setViewingUser(result)}>{result.displayname}</Link><Button  floated='right' onClick={()=>this.requestFollow(result)} >Requested</Button></List.Header></List.Item>
-      ))}
+  
+  results = () => {
+    let baseUrl = this.props.baseUrl
+    return (
+      <List size='small'>
+        {this.state.results.map(
+          (result =>
+            <List.Item>
+              <List.Header>
+                <Image src={baseUrl + result.avatar.url} className='user-avatar' avatar />
+                <Link to='/profile' onClick={() => this.props.setViewingUser(result)}>
+                  {result.displayname}
+                </Link>
+              </List.Header>
+            </List.Item>
+          )
+        )}
       </List>
-
+    )
   }
 
-  render(){
+  render() {
 
     return <>
-    <Popup
-    on='focus'
-    trigger={<input className="search-bar" type='text' onKeyUp={this.handleChange} value={this.state.name} placeholder='Search Users'/>}
-    >
-    {this.state.noRelation.length>0 || this.state.following.length>0 || this.state.requested.length>0 ?
-      <div className='collection'>
-     {this.results()}
-    </div> : null}
-    </Popup>
+      <Popup
+        on='focus'
+        trigger={<input className="search-bar" type='text' onKeyUp={this.handleChange} value={this.state.name} placeholder='Search Users' />}
+      >
+          <div className='collection'>
+            {this.state.results && this.results()}
+          </div>
+      </Popup>
     </>
 
   }
